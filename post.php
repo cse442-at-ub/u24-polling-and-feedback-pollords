981521dbecf6fdabb80795f31f75d285a2ddbc35
Angel <Colonad@sunypoly.edu>
2024-06-03 18:52:00 -0400

    Task for Tuesday

--- post.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$data = json_decode(file_get_contents('php://input'), true);
-echo json_encode(["message" => "This is the POST response", "received" => $data]);
-?>
