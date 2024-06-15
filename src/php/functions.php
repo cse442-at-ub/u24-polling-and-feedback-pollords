5eac2e041657c82be130355f1ae31bde5ac66258
JJZ43 <jjzalot5@gmail.com>
2024-06-15 14:37:10 -0400

    Made function and tests for account checking #27
    The function will check if an item is present in the database, returning a true/false for success, and a string message for details.
    In the tests folder, it passes all the checkUser tests created.

---
+++ src/php/functions.php
@@ -0,0 +1,54 @@
+<?php
+
+
+include("connection.php");
+
+
+function checkUser($email, $pass)
+{
+    //echo $email;
+    //echo $pass;
+    if(empty($email) || empty($pass))
+    {
+        return array(false,"Error: Don't leave inputs empty");
+    }
+
+    $conn=$_SESSION['conn'];
+
+    if(!isset($conn->server_info)){
+        return array(false,"Error: Login is currently down, please try again later");
+    }
+
+
+
+    $query = "select * from userAccs where email = '$email' limit 1";
+    $result = mysqli_query($conn, $query);
+
+    $temp = strpos($email,"@buffalo.edu");
+    if($temp === false){
+        return array(false,"Error: Must be a valid buffalo.edu email");
+    }
+    if($temp == 0 || strlen($email)!=$temp+12){
+        return array(false,"Error: Must be a valid buffalo.edu email");
+    }
+
+
+    if($result) {
+        if ($result && mysqli_num_rows($result) > 0) {
+            $db_pass = mysqli_fetch_assoc($result)['password'];
+
+            if (password_verify($pass, $db_pass)){
+                // Deal with successful login
+                return array(true,"Success: User Exists with same login and password");
+            }
+            else {
+                // Handle authentication error
+                return array(false,"Error: Information wrong or User does not exist");
+
+            }
+        }
+    }
+    return array(false,"Error: Information wrong or User does not exist");
+}
+
+
---
+++ src/tests/checkUser/wrongPass.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+include($_SERVER['DOCUMENT_ROOT'].'/u24-polling-and-feedback-pollords/src/php/functions.php');
+
+list($passed,$message) = checkUser("fortnite@buffalo.edu","wrongpass");
+echo $message;
