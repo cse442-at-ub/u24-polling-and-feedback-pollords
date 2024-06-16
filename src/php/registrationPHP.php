d28941bd6dcd1f1acd9f9a7f768f6ca35432b329
RYLZLionBlaze <shunbita@buffalo.edu>
2024-06-16 16:21:29 -0400

    added registration post request handler and test files that test for varying inputs.

---
+++ src/php/registrationPHP.php
@@ -0,0 +1,30 @@
+<?php
+function registrationPost($username, $password) {
+    if (empty($username) || empty($password)) {
+        return false;
+    }
+    if (!preg_match('/^(?=.*[!@#$%^&*])/', $password)) {
+        return false;
+    }
+    return true;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $result = registrationPost($username, $password);
+    if ($result) {
+        echo json_encode([
+            'Method' => 'POST',
+            'Path' => 'register',
+            'Body' => 'username=' . $username . ', password=' . $password
+        ]);
+    } else {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid input']);
+    }
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+}
+?>
