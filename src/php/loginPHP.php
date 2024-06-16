297fb0d0db27dc79c1de4b124682bc214a6925b3
RYLZLionBlaze <shunbita@buffalo.edu>
2024-06-16 16:30:31 -0400

    Added loginPostrequest php handler with html test files on varying inputs.

---
+++ src/php/loginPHP.php
@@ -0,0 +1,30 @@
+<?php
+function loginPost($username, $password) {
+    if (empty($username) || empty($password)) {
+        return false;
+    }
+    if (!preg_match('/^(?=.*[!@#$%^&*])/', $password)) {
+        return false;
+    }
+    return true;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $result = loginPost($username, $password);
+    if ($result) {
+        echo json_encode([
+            'Method' => 'POST',
+            'Path' => 'login',
+            'Body' => 'username=' . $username . ', password=' . $password
+        ]);
+    } else {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid input']);
+    }
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+}
+?>
