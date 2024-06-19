ef66058c4d1b740531c5a51580232211f1a14efb
JJZ43 <jjzalot5@gmail.com>
2024-06-19 15:56:28 -0400

    Merge branch 'handle-login-response-49' into dev

--- src/js/index.js
+++ src/js/index.js
@@ -37,7 +37,16 @@ async function submitLogin() {
 }
 
 function handleResponse(data){
+  console.log(data)
+  if (data.success) {
+    localStorage.setItem("userEmail", data.token);
 
+    window.location.href = `main.html?user=${data.token}`;
+  } else {
+    const errorElement = document.getElementById(`errorLogin`);
+    errorElement.innerHTML = data.message;
+    errorElement.style.display = "block";
+  }
 }
 
 
