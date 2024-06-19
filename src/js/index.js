4fdd7e0a9780040ee0763068f39195fa118ea4a9
JJZ43 <jjzalot5@gmail.com>
2024-06-19 15:47:19 -0400

    Revert "implemented placeholder dashboard page, cleaned up tests, added proper response handler in index.js #49"
    This reverts commit 49c516b265d68af3c78eff2ddc135f17bc8d58b0.

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,44 @@
+
 // Function to handle login data submission
 async function submitLogin() {
+
   var addr = document.getElementById("emailLogin").value;
   var pw = document.getElementById("passwordLogin").value;
 
   const formData = new FormData();
-  formData.append("email", addr);
-  formData.append("password", pw);
-  console.log(formData);
+  formData.append("email",addr)
+  formData.append("password",pw)
+  //console.log(loginForm)
+  console.log(formData)
+
 
   fetch("php/login-user.php", {
-      method: "post",
-      body: formData,
+    method: "post",
+    body: formData,
   })
-      .then((response) => {
-          if (!response.ok) {
-              throw new Error("Network response was not ok");
-          }
-          return response.json();
-      })
-      .then((data) => {
-          handleLoginResponse(data);
-      })
-      .catch((error) => {
-          console.error("Error sending data to the backend:", error);
-      });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      //console.log(response)
+      //console.log(response.text())
+      let temp = response.json()
+      //console.log(temp)
+      return temp;
+    })
+
+
+    .then((data) => {
+      handleResponse(data)
+    })
+    .catch((error) => {
+      console.error("Error sending data to the backend:", error);
+    });
 }
 
-function handleLoginResponse(response) {
-  if (response.status === 200) {
-      // Handle successful login
-      localStorage.setItem('authToken', response.token);
-      window.location.href = "dashboard.html";
-  } else if (response.status === 401) {
-      // Handle invalid credentials
-      const errorMessage = document.querySelector('.error-message');
-      errorMessage.innerText = response.message;
-      errorMessage.style.display = 'block';
-  } else if (response.status === 500) {
-      // Handle server error
-      const errorMessage = document.querySelector('.error-message');
-      errorMessage.innerText = response.message;
-      errorMessage.style.display = 'block';
-  }
+function handleResponse(data){
+
 }
+
+
+
