179e95d1ea5456307aac3e8cb3315cd7f56af452
aolansen <aolansen@buffalo.edu>
2024-06-21 18:41:16 -0400

    Added some error handling and adjusted title text to proper position

--- src/js/courseCreation.js
+++ src/js/courseCreation.js
@@ -1,3 +1,18 @@
 function backFunc() {
     window.location.href = `main.html`;
 }
+
+function errorCatcher() {
+    var name = document.getElementById("courseName").value;
+    var code = document.getElementById("courseCode").value;
+    var sem = document.getElementById("semester").value;
+    var instrs = document.getElementById("instructorEmails").value;
+    var err = document.getElementById("error");
+
+    if (name === "" || code === "" || sem === "") {
+        err.textContent = "Please fill out all four fields";
+    }
+    else if (instrs === "") {
+        err.textContent = "Please provide at least one instructor email for this course";
+    }
+}
