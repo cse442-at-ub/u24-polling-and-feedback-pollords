716e5f384ea17820ce3f045f8c64e2f7f35bb450
aolansen <aolansen@buffalo.edu>
2024-06-26 18:37:52 -0400

    added barebones sql insertion absent meaningful error/security checks

--- src/php/studentFeedback.php
+++ src/php/studentFeedback.php
@@ -8,5 +8,21 @@ include 'connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $conn=$_SESSION['conn'];
 
-    $
+    $courseID = mysqli_real_escape_string($conn,$_POST['courseID']);
+    $studentID = mysqli_real_escape_string($conn,$_POST['studentID']);
+    $response = mysqli_real_escape_string($conn,$_POST['response']);
+    $timeUpdated = mysqli_real_escape_string($conn,$_POST['timeUpdated']);
+
+    if(isset($conn->server_info)) {
+        if (empty($courseId) || empty($studentId) || empty($response) || empty($timeUpdated)) {
+            $conn->close();
+            echo json_encode(array("success" => false, "message" => "Error: Unable to collect required data","id"=>-1));
+            exit();
+        }
+
+        $query = "insert into feedbackAnswers (courseID, studentID, term, response, timeUpdated) values ('$courseID', '$studentID', '$response', '$timeUpdated')";
+        mysqli_query($conn, $query);
+        echo json_encode(array("success" => true, "message" => "Success: Feedback sent correctly","id"=>$studentID));
+        $conn->close();
+    }
 }
