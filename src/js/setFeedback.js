1b4a371d143818739c8d00a3736a785f73b7c1e0
jacob <jastein2@buffalo.edu>
2024-06-27 08:37:10 -0400

    Updated setFeedback to reload on success

--- src/js/setFeedback.js
+++ src/js/setFeedback.js
@@ -5,18 +5,20 @@ async function setFeedback(action) {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({action })
+            body: JSON.stringify({ action })
         });
 
         const data = await response.json();
 
-        if (data.status === 'success') {
+        if (data.status === 1) {
             alert(data.message);
+            location.reload();
         } else {
             alert(data.message);
         }
-        if(data !== 'true' && data !== 'false'){
-            console.log('Invalid Request, must be true or false');
+
+        if (data.status !== 1 && data.status !== 0) {
+            console.log('Invalid Request, must be 1 or 0');
         }
     } catch (error) {
         console.error('Error:', error);
