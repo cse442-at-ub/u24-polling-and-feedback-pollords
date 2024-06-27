8e0bb0cfc3c6313a286d60946d94e5971b378786
aolansen <aolansen@buffalo.edu>
2024-06-26 22:00:47 -0400

    Reformatted code for better error checking/security. Hopefully close to complete

--- src/php/studentFeedback.php
+++ src/php/studentFeedback.php
@@ -1,32 +1,155 @@
 <?php
+//ob_end_clean();
+//ob_start();
+
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
 session_start();
-include 'connection.php';
+include("connection.php");
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $conn=$_SESSION['conn'];
 
-    $courseID = mysqli_real_escape_string($conn,$_POST['courseID']);
-    //$studentID = mysqli_real_escape_string($conn,$_POST['studentID']);
-    $studentID = $_SESSION['userID'];
-    $response = mysqli_real_escape_string($conn,$_POST['response']);
-    //$timeUpdated = mysqli_real_escape_string($conn,$_POST['timeUpdated']);
+    if (isset($_SESSION['token']) && isset($_SESSION['instructor']) && isset($_SESSION['userID'])) {
+        $instr = $_SESSION['instructor'];
+        $token = $_SESSION['token'];
+        $userID = $_SESSION['userID'];
+        $courseID = $_POST['courseID'];
+        $query = $conn->prepare("select * from tokens where token = ? limit 1");
+        $query->bind_param("s", $token);
+        $query->execute();
+        $result = $query->get_result();
+        if($result) {
+            if ($result && mysqli_num_rows($result) == 0) {
+                $conn->close();
+                //header("Location: ../index.html");
+                //exit;
+                echo json_encode(array("instructor" => -1,"message"=>"Error: You are not authorized, try logging in again","feedbackOpen"=>0));
+                return;
+            } else {
+                $query = $conn->prepare("select * from courses where id = ? limit 1");
+                $query->bind_param("i", $courseID);
+                $query->execute();
+                $result = $query->get_result();
+                if($result) {
+                    if ($result && mysqli_num_rows($result) == 0) {
+                        $conn->close();
+                        if($instr==0){
+                            echo json_encode(array("instructor" => -1, "message" => "Error: Course does not exist", "feedbackOpen" => 0));
+                            return;
+                            //header("Location: ../mainStud.html", true,  301);
+                            //exit;
+                        } else {
+                            echo json_encode(array("instructor" => -1, "message" => "Error: Course does not exist", "feedbackOpen" => 0));
+                            return;
+                            //header("Location: ../main.html", true,  301);
+                            //exit;
 
-    if(isset($conn->server_info)) {
-        if (empty($courseId) || empty($studentId) || empty($response)) {
+                        }
+
+                    } else {
+                        $temp = "";
+                        if($instr==0){
+                            $temp = mysqli_fetch_assoc($result)['students'];
+                        } else {
+                            $temp = mysqli_fetch_assoc($result)['instructors'];
+                        }
+                        if($temp==""){
+                            $conn->close();
+                            if($instr==0){
+                                echo json_encode(array("instructor" => -1, "message" => "Error: You are not a member of this course", "feedbackOpen" => 0));
+                                return;
+                                //header("Location: ../mainStud.html", true,  301);
+                                //exit;
+                            } else {
+                                echo json_encode(array("instructor" => -1, "message" => "Error: You are not a member of this course", "feedbackOpen" => 0));
+                                return;
+                                //header("Location: ../main.html", true,  301);
+                                //exit;
+                            }
+                            //echo json_encode(array("instructor" => -1, "message" => "Error: You are not a member of this course", "feedbackOpen" => 0));
+                            //return;
+                        }
+                        $members = explode(',', $temp);
+                        $isMember = false;
+                        foreach ($members as $curr) {
+                            if($curr==$userID){
+                                $isMember = true;
+                            }
+                        }
+                        if(!$isMember){
+                            $conn->close();
+                            if($instr==0){
+                                echo json_encode(array("instructor" => -1, "message" => "Error: You are not a member of this course", "feedbackOpen" => 0));
+                                return;
+                                //header("Location: ../mainStud.html", true,  301);
+                                //exit;
+                            } else {
+                                echo json_encode(array("instructor" => -1, "message" => "Error: You are not a member of this course", "feedbackOpen" => 0));
+                                return;
+                                //header("Location: ../main.html", true,  301);
+                                //exit;
+                            }
+                            //echo json_encode(array("instructor" => -1, "message" => "Error: You are not a member of this course", "feedbackOpen" => 0));
+                            //return;
+                        }
+                        $query = $conn->prepare("select * from courses where id = ? limit 1");
+                        $query->bind_param("i", $courseID);
+                        $query->execute();
+                        $result = $query->get_result();
+                        $feedbackOpen = mysqli_fetch_assoc($result)['feedbackOpen'];
+                        if (empty($courseId) || empty($studentId) || empty($response)) {
+            		    $conn->close();
+            		    echo json_encode(array("success" => false, "message" => "Error: Unable to collect required data","id"=>-1));
+            		    exit();
+        		}
+        		if ($response > 5 || $response < 1) {
+            		    $conn->close();
+            		    echo json_encode(array("success" => false, "message" => "Error: Invalid response value","id"=>-1));
+            		    exit();
+        		}
+
+        		$preppedQuery = $conn->prepare("INSERT INTO feedbackAnswers (courseID, studentID, response) value (?, ?, ?)");
+        		$preppedQuery->bind_param("sss", $courseID, $studentID, $response);
+        		$preppedQury->execute();
+        		//mysqli_query($conn, $preppedQuery);
+        		echo json_encode(array("success" => true, "message" => "Success: Feedback sent correctly","id"=>$studentID)); // I am a titan of PHP
+        		$conn->close();
+
+                    }
+                } else {
+                    $conn->close();
+                    if($instr==0){
+                        echo json_encode(array("instructor" => -1, "message" => "Error: Course does not exist", "feedbackOpen" => 0));
+                        return;
+                        //header("Location: ../mainStud.html", true,  301);
+                        //exit;
+                    } else {
+                        echo json_encode(array("instructor" => -1, "message" => "Error: Course does not exist", "feedbackOpen" => 0));
+                        return;
+                        //header("Location: ../main.html", true,  301);
+                        //exit;
+                    }
+                    //echo json_encode(array("instructor" => -1, "message" => "Error: Course does not exist", "feedbackOpen" => 0));
+                    //return;
+                }
+            }
+        } else {
             $conn->close();
-            echo json_encode(array("success" => false, "message" => "Error: Unable to collect required data","id"=>-1));
-            exit();
-        }
+            //header("Location: ../index.html", true,  301);
+            //exit;
+            echo json_encode(array("instructor" => -1,"message"=>"Error: You are not authorized, try logging in again","feedbackOpen"=>0));
+            return;
 
-        // replace below with prepared statement
-        //$query = "insert into feedbackAnswers (courseID, studentID, response) values ('$courseID', '$studentID', '$response')";
-        //mysqli_query($conn, $query);
-        $preppedQuery = conn->prepare("INSERT INTO feedbackAnswers (courseID, studentID, response) value ('$courseID', '$studentID', '$response')");
-        mysqli_query($conn, $preppedQuery);
-        echo json_encode(array("success" => true, "message" => "Success: Feedback sent correctly","id"=>$studentID)); // I am a titan of PHP
+        }
+    } else {
         $conn->close();
+        //header("Location: ../index.html", true,  301);
+        //exit;
+        echo json_encode(array("instructor" => -1,"message"=>"Error: You are not authorized, try logging in again","feedbackOpen"=>0));
+        return;
     }
 }
