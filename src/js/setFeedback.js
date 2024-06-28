d13ed9f6ec54231102df82d53f706d132b831461
JJZ43 <jjzalot5@gmail.com>
2024-06-28 13:36:51 -0400

    Updated syntax for merge into dev

--- src/js/setFeedback.js
+++ src/js/setFeedback.js
@@ -1,27 +1,31 @@
-async function setFeedback(action) {
-    try {
-        const response = await fetch('setFeedback.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ action })
-        });
+async function setFeedback(action,courseID) {
+    const formData = new FormData();
+    formData.append("action",action)
+    formData.append("courseID",courseID)
+    if (action != 1 && action != 0) {
+        console.log('Invalid Request, must be 1 or 0');
+    } else {
+        try {
+            const response = await fetch('../php/setFeedback.php', {
+                method: 'POST',
+                body: formData,
+            });
 
-        const data = await response.json();
+            const data = await response.json();
 
-        if (data.status === 1) {
-            alert(data.message);
-            location.reload();
-        } else {
-            alert(data.message);
+            if (data.status == 1) {
+                console.log(data.message);
+                //location.reload();
+            } else {
+                console.log(data.message);
+            }
+
+
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred while processing your request.');
         }
-
-        if (data.status !== 1 && data.status !== 0) {
-            console.log('Invalid Request, must be 1 or 0');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        alert('An error occurred while processing your request.');
     }
 }
+
+
